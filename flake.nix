55df14279724b365ae399480672b91e13025b560
Ivan Subotic <400790+subotic@users.noreply.github.com>
2024-04-28 13:19:41 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,11 +10,21 @@
     in
     {
       devShells.default = pkgs.mkShell {
+
+        shellHook = ''
+            # echo "C++ includes: $(clang++ -E -x c++ - -v < /dev/null 2>&1 | grep '/include')"
+            # echo | clang++ -v -E -x c++ -
+            # echo | g++ -Wp,-v -x c++ - -fsyntax-only
+            export PS1="\\u@\\h | nix-develop> "
+        '';
         packages = with pkgs; [
           rustup
           clang
           protobuf
-        ];
+        ] ++ (pkgs.lib.optional stdenv.isDarwin [
+          pkgs.iconv
+          darwin.apple_sdk.frameworks.Foundation
+        ]);
 
         LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
       };
